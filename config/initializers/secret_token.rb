268d41d6518ed122b0355ed5e6eb81bff868641f
iraneira <ira-neira@mail.ru>
2016-10-03 14:12:07 +0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstAppOctober::Application.config.secret_key_base = '068349d99eab4c93dfba19d0e432067253a69b99cec57d51ea5f9a9ec5fd54f060f3ce672aef2db13ee029adb8cbcbc11900329ee91323d0828d197a2df48e0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstAppOctober::Application.config.session_store :cookie_store, key: '_first_app_october_session'
